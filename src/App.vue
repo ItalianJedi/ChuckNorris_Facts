ceb6296c7ff22246918dbfafe6eb0c5bb7ae1f7d
Tony Graf <tony.graf@gmail.com>
2020-07-17 12:14:15 -0700

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header" :style="{backgroundImage:`url(${backgroundUrl})`}">
+      <div class="text-box">
+        <h1 class="heading-primary">
+          <span class="heading-primary-main">Chuck Norris Facts!</span>
+        </h1>
+        <chuck-norris />
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChuckNorris from "./components/ChuckNorris.vue";
+import backgroundUrl from "./assets/chucknorris.png";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      backgroundUrl
+    };
+  },
   components: {
-    HelloWorld
+    ChuckNorris
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.heading-primary-main {
+  font-family: "Londrina Shadow", cursive;
+  font-size: 80px;
+  color: #393c3f;
+  font-weight: bold;
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+body {
+  background: #818182;
+  /*background: #4f5051;*/
+  padding: 30px;
+}
+
+.header {
+  height: 95vh;
+  background-image: linear-gradient(
+    to right bottom,
+    rgba(110, 179, 239, 0.795),
+    rgba(43, 104, 158, 0.808)
+  );
+  background-repeat: no-repeat;
+  background-position: top;
+  clip-path: polygon(0 0, 100% 0, 100% 87%, 0 100%);
 }
 </style>
---
+++ src/components/ChuckNorris.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <button v-on:click="norrisJoke()">Click Me</button>
+    </div>
+    <spinner v-if="showSpinner"></spinner>
+    <div class="joke" v-if="currentJoke" v-html="currentJoke.joke">{{currentJoke.joke}}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import CubeSpinner from "@/components/CubeSpinner";
+
+export default {
+  name: "ChuckNorris",
+  components: {
+    spinner: CubeSpinner
+  },
+  data() {
+    return {
+      currentJoke: null,
+      showSpinner: false
+    };
+  },
+  methods: {
+    norrisJoke() {
+      this.showSpinner = true;
+      axios.get("https://api.icndb.com/jokes/random").then(response => {
+        this.showSpinner = false;
+        this.currentJoke = response.data.value;
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* eslint-disable */
+.wrapper {
+  padding-top: 400px;
+  text-align: center;
+}
+
+button {
+  position: relative;
+  top: 50%;
+  font-family: "Londrina Shadow", cursive;
+  font-weight: bold;
+  opacity: 0.9;
+  background-color: #393c3f;
+  border: ridge gray;
+  border: none;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+.joke {
+  font-size: 15px;
+  font-weight: bold;
+  border: ridge gray;
+  width: 350px;
+  padding: 10px;
+  margin: 60px;
+  margin-top: 95px;
+  margin-left: auto;
+  margin-right: auto;
+  background: #edf4fa;
+}
+
+/*Phone screens*/
+@media only screen and (min-width: 320px) and (max-width: 700px) and (-webkit-min-device-pixel-ratio: 2) {
+  .joke {
+    font-size: 10px;
+    font-weight: bold;
+    border: ridge gray;
+    width: 250px;
+    padding: 10px;
+    margin: 60px;
+    margin-top: 35px;
+    margin-left: auto;
+    margin-right: auto;
+    background: #edf4fa;
+  }
+}
+
+/*Tablet Screens*/
+@media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 1) {
+  button {
+    margin-top: 500px;
+  }
+  .joke {
+    font-size: 10px;
+    font-weight: bold;
+    border: ridge gray;
+    width: 250px;
+    padding: 10px;
+    margin: 90px;
+    margin-top: 95px;
+    margin-left: auto;
+    margin-right: auto;
+    background: #edf4fa;
+  }
+}
+</style>
